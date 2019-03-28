ab218534d476c19d07e86649ecc6264736e6ed2c
hg <hgkang822@gmail.com>
2019-03-29 08:48:59 +0900

    190329

--- algo/최소생산비용.py
+++ algo/최소생산비용.py
@@ -70,17 +70,17 @@ for tc in range(1, T + 1):
     print(low)
 
 def charge(start):
-global change,low
-if start>=battery[0]:
-if low>change:
-low=change
-return
-if change>low:
-return
-for i in range(battery[start],0,-1):
-change+=1
-charge(start+i)
-change-=1
+    global change,low
+    if start>=battery[0]:
+        if low>change:
+            low=change
+        return
+    if change>low:
+        return
+    for i in range(battery[start],0,-1):
+        change+=1
+        charge(start+i)
+        change-=1
 
 T=int(input())
 for tc in range(T):
@@ -128,3 +128,77 @@ visited[0]=1
 result=[0]*(V+1)
 prim()
 print("#%d %d"%(tc+1,sum(result)))
+
+def findset(x):
+    if x==p[x]:
+        return x
+    else:
+        return findset(p[x])
+def union(x,y):
+    p[findset(y)]=findset(x)
+
+T=int(input())
+for tc in range(1,T+1):
+    N,M=map(int,input().split())
+    data1=list(map(int,input().split()))
+    data=[0]*M
+    p=[0]*(N+1)
+    for i in range(M):
+        if data1[i*2]>data1[i*2+1]:
+            data[i]=(data1[i*2+1],data1[i*2])
+        else:
+            data[i]=(data1[i*2],data1[i*2+1])
+
+    for i in range(N+1):
+        p[i]=i
+
+    for i in range(len(data)):
+        union(data[i][0],data[i][1])
+
+    for i in range(1, N + 1):
+        p[i]=findset(p[i])
+
+    cnt=0
+    checker=[0]*(N+1)
+    for i in range(1,N+1):
+        if checker[p[i]]!=1:
+            checker[p[i]]=1
+            cnt+=1
+
+    print('#%d %d' %(tc, cnt))def findset(x):
+    if x==p[x]:
+        return x
+    else:
+        return findset(p[x])
+def union(x,y):
+    p[findset(y)]=findset(x)
+
+T=int(input())
+for tc in range(1,T+1):
+    N,M=map(int,input().split())
+    data1=list(map(int,input().split()))
+    data=[0]*M
+    p=[0]*(N+1)
+    for i in range(M):
+        if data1[i*2]>data1[i*2+1]:
+            data[i]=(data1[i*2+1],data1[i*2])
+        else:
+            data[i]=(data1[i*2],data1[i*2+1])
+
+    for i in range(N+1):
+        p[i]=i
+
+    for i in range(len(data)):
+        union(data[i][0],data[i][1])
+
+    for i in range(1, N + 1):
+        p[i]=findset(p[i])
+
+    cnt=0
+    checker=[0]*(N+1)
+    for i in range(1,N+1):
+        if checker[p[i]]!=1:
+            checker[p[i]]=1
+            cnt+=1
+
+    print('#%d %d' %(tc, cnt))
